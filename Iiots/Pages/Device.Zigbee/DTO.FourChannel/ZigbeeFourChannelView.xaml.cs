74495b95c1e51ad246a407073c2026d3034b7342
lfzrg <lfzrg@DESKTOP-285N53Q>
2020-10-31 16:23:37 +0800

    Zigbee四通道数据多方式呈现

---
+++ Iiots/Pages/Device.Zigbee/DTO.FourChannel/ZigbeeFourChannelView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Iiots.Pages
+{
+    
+    public partial class ZigbeeFourChannelView : UserControl
+    {
+        public ZigbeeFourChannelView()
+        {
+            InitializeComponent();
+            __Cmb_ChannelType.SelectedIndex = 0;
+        }
+    }
+}
